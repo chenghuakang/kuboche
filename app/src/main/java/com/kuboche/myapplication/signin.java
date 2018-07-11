06eaf92f21952f09747f4880a0bce5b238778961
chenghuakang <chenghuakang@gmail.com>
2018-07-11 19:43:43 +0800

    add english

--- app/src/main/java/com/kuboche/myapplication/signin.java
+++ app/src/main/java/com/kuboche/myapplication/signin.java
@@ -5,6 +5,10 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.kuboche.bean.User;
 
 public class signin extends Activity {
     @Override
@@ -14,12 +18,25 @@ public class signin extends Activity {
     }
     public void signin1(View v){
         Intent i = new Intent();
-        i.setClassName(getApplicationContext(),"com.kuboche.myapplication.framework");
-        startActivity(i);
+        EditText editText1=(EditText)findViewById(R.id.account);
+        String account = editText1.getText().toString();
+        EditText editText2=(EditText)findViewById(R.id.pwd1);
+        String pwd1 = editText2.getText().toString();
+        EditText editText3=(EditText)findViewById(R.id.pwd2);
+        String pwd2 = editText3.getText().toString();
+        if(passwordIsRight(pwd1,pwd2)){
+            User U1 = new User(account,null,pwd1,"abc","abc");
+            U1.save();
+            i.setClassName(getApplicationContext(),"com.kuboche.myapplication.framework");
+            startActivity(i);
+            Toast.makeText(this,"注册成功",Toast.LENGTH_LONG).show();}
     }
     public void quit1(View v){
         Intent i = new Intent();
         i.setClassName(getApplicationContext(),"com.kuboche.myapplication.start");
         startActivity(i);
+    }
+    private boolean passwordIsRight(String pwd1,String pwd2){
+        return pwd1.equals(pwd2);
     }
 }
--- app/src/main/java/com/kuboche/myapplication/start.java
+++ app/src/main/java/com/kuboche/myapplication/start.java
@@ -7,16 +7,39 @@ import android.content.res.Resources;
 import android.os.Bundle;
 import android.util.DisplayMetrics;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.Toast;
 
+import com.kuboche.bean.PackRecord;
+import com.kuboche.bean.User;
+
 import java.time.Instant;
+import java.util.Date;
+import java.util.List;
 import java.util.Locale;
 
 public class start extends Activity {
+
+    public void main() {
+        User U1;
+        U1 = new User("18171370371", "可可", "123456", "爸爸的生日", "8月7日");
+        U1.save();
+        U1 = new User("18171370372", "小可可", "1234567", "爸爸的生日", "8月7日");
+        U1.save();
+        U1 = new User("18171370373", "大可可", "12345678", "爸爸的生日", "8月7日");
+        U1.save();
+        U1 = new User("18171370378", "大可可", "12345678", "爸爸的生日", "8月7日");
+        U1.save();
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.start);
+        main();
+
+        PackRecord p = new PackRecord("111", "njnsajdk", 6.646, new Date());
+        p.save();
     }
 
     @Override
@@ -24,48 +47,56 @@ public class start extends Activity {
         super.onPause();
     }
 
-    public void logIn(View v){
+    public void logIn(View v) {
         Intent i = new Intent();
-        if(passwordIsRight("password","password")) {
-        i.setClassName(getApplicationContext(),"com.kuboche.myapplication.framework");
-        startActivity(i);
-        /*setContentView(R.layout.framework);*/
-        }
-        else{
-            Toast.makeText(this,"密码错误",Toast.LENGTH_LONG).show();
+        EditText editText1 = (EditText) findViewById(R.id.account);
+        String account = editText1.getText().toString();
+        EditText editText2 = (EditText) findViewById(R.id.password);
+        String password = editText2.getText().toString();
+
+        List<User> userList = User.find(User.class, "account=? and password=?", account, password);
+        if (userList.size() > 0) {
+            i.setClassName(getApplicationContext(), "com.kuboche.myapplication.framework");
+            startActivity(i);
+            /*setContentView(R.layout.framework);*/
+        } else {
+            Toast.makeText(this, "密码错误", Toast.LENGTH_LONG).show();
         }
     }
-    public void signIn(View v){
-        Intent i =new Intent();
+
+    public void signIn(View v) {
+        Intent i = new Intent();
         i.setClassName(getApplicationContext(), "com.kuboche.myapplication.signin");
         startActivity(i);
     }
-    public void password1(View v){
-        Intent i =new Intent();
-        i.setClassName(getApplicationContext(),"com.kuboche.myapplication.change_password1");
+
+    public void password1(View v) {
+        Intent i = new Intent();
+        i.setClassName(getApplicationContext(), "com.kuboche.myapplication.change_password1");
         startActivity(i);
     }
-    private boolean passwordIsRight(String password1,String password2){
+
+    private boolean passwordIsRight(String password1, String password2) {
         return password1.equals(password2);
     }
-    public void changeLanguage(View v){
-        Resources r= getBaseContext().getResources();
-        DisplayMetrics DM=r.getDisplayMetrics();
+
+    public void changeLanguage(View v) {
+        Resources r = getBaseContext().getResources();
+        DisplayMetrics DM = r.getDisplayMetrics();
         Configuration config = r.getConfiguration();
-        if(config.locale == Locale.ENGLISH){
-        config.locale= Locale.CHINESE;
-        r.updateConfiguration(config,DM);
-        Intent i =new Intent();
-        i.setClassName(getApplicationContext(),"com.kuboche.myapplication.start");
-        startActivity(i);
-        }
-        else {
-            config.locale= Locale.ENGLISH;
-            r.updateConfiguration(config,DM);
-            Intent i =new Intent();
-            i.setClassName(getApplicationContext(),"com.kuboche.myapplication.start");
+        if (config.locale == Locale.ENGLISH) {
+            config.locale = Locale.CHINESE;
+            r.updateConfiguration(config, DM);
+            Intent i = new Intent();
+            i.setClassName(getApplicationContext(), "com.kuboche.myapplication.start");
+            startActivity(i);
+        } else {
+            config.locale = Locale.ENGLISH;
+            r.updateConfiguration(config, DM);
+            Intent i = new Intent();
+            i.setClassName(getApplicationContext(), "com.kuboche.myapplication.start");
             startActivity(i);
         }
-
     }
 }
+
