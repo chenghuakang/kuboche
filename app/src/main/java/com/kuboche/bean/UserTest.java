7e65411b4d81970b9d9072a26616986c45f1c253
chenghuakang <chenghuakang@gmail.com>
2018-07-11 19:44:33 +0800

    delete

--- app/src/main/java/com/kuboche/bean/UserTest.java
+++
@@ -1,9 +0,0 @@
-package com.kuboche.bean;
-
-public class UserTest{
-    public void main() {
-        User U1;
-        U1 = new User("18171370371", "可可", "123456", "爸爸的生日", "8月7日");
-        U1.save();
-    }
-}
