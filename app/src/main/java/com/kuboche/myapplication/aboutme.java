7d9c98bf99605ae4ee76ee85b63edc1481c88966
chenghuakang <chenghuakang@gmail.com>
2018-07-13 19:11:49 +0800

    a large updata

--- app/src/main/java/com/kuboche/myapplication/aboutme.java
+++ app/src/main/java/com/kuboche/myapplication/aboutme.java
@@ -19,28 +19,30 @@ import java.time.Instant;
 import java.util.Date;
 import java.util.List;
 
-public class aboutme extends Activity implements AdapterView.OnItemClickListener{
+public class aboutme extends Activity implements AdapterView.OnItemClickListener {
     ListView listView;
     ArrayAdapter<String> adapter;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.aboutme);
         //要显示的数据
-        String[] strs = {"我的账户","停车记录","账户信息","注销账户"};
+        String[] strs = {"我的账户", "停车记录", "账户信息", "注销账户"};
         //创建ArrayAdapter
         adapter = new ArrayAdapter<String>
-                (this,android.R.layout.simple_expandable_list_item_1,strs);
+                (this, android.R.layout.simple_expandable_list_item_1, strs);
         //获取ListView对象，通过调用setAdapter方法为ListView设置Adapter设置适配器
         listView = (ListView) findViewById(R.id.list_view);
         listView.setAdapter(adapter);
         listView.setOnItemClickListener(this);
 
     }
-    public String[] add(String[] strs, String str){
-        String[] s =  new String[strs.length+1];
-         int i = 0;
-        for(String ax:strs){
+
+    public String[] add(String[] strs, String str) {
+        String[] s = new String[strs.length + 1];
+        int i = 0;
+        for (String ax : strs) {
             s[i] = ax;
             i++;
         }
@@ -61,60 +63,46 @@ public class aboutme extends Activity implements AdapterView.OnItemClickListener
             PackRecord p3 = PackRecord.findById(PackRecord.class, 1);
             List<PackRecord> p2 = PackRecord.listAll(PackRecord.class);
 
-<<<<<<< HEAD
-        switch((int)l){
-            case 0:
-                List<User> us = User.listAll(User.class);
-                if(!us.isEmpty()){
-                    User us1 = us.get(0);
-                String[] strs1 = {"账号： "+us1.getAccount(),"\n"+us1.getNickname()};
-                ArrayAdapter<String> adapter1 = new ArrayAdapter<String>
-                        (this,android.R.layout.simple_expandable_list_item_1,strs1);
-                listView.setAdapter(adapter1);}
-                else{
-                    Toast.makeText(this, "未成功登录", Toast.LENGTH_LONG).show();
+
+                switch ((int) l) {
+                    case 0:
+                        List<User> us = User.listAll(User.class);
+                        if (!us.isEmpty()) {
+                            User us1 = us.get(0);
+                            String[] strs1 = {"账号： " + us1.getAccount(), "\n" + us1.getNickname()};
+                            ArrayAdapter<String> adapter1 = new ArrayAdapter<String>
+                                    (this, android.R.layout.simple_expandable_list_item_1, strs1);
+                            listView.setAdapter(adapter1);
+                        } else {
+                            Toast.makeText(this, "账号不存在", Toast.LENGTH_LONG).show();
+                        }
+                        break;
+                    case 1:
+                        String[] strs2 = new String[p2.size()];
+                        for (int j = 0; j < p2.size(); j++) {
+                            strs2[j] = "No" + j + "    " + p2.get(j).getName() + "  " + p2.get(j).getParkId()
+                                    + "号停车场\n" + p2.get(j).getDate().toString();
+                        }
+                        ArrayAdapter<String> adapter2 = new ArrayAdapter<String>
+                                (this, android.R.layout.simple_expandable_list_item_1, strs2);
+                        listView.setAdapter(adapter2);
+                        break;
+                    case 2:
+                        break;
+                    case 3:
+                        Intent sendIntent = new Intent(Intent.ACTION_SENDTO, Uri.parse("smsto:" + "10086"));
+                        //sendIntent.setPackage("com.android.mms");
+                        sendIntent.putExtra("sms_body", "康程华要在马路上停车");
+                        startActivity(sendIntent);
+                        break;
+                    case 4:
+
+                        break;
+
+                    default:
                 }
-                break;
-=======
-            switch ((int) l) {
-                case 0:
-                    List<User> us = User.listAll(User.class);
-                    if (!us.isEmpty()) {
-                        User us1 = us.get(0);
-                        String[] strs1 = {"账号： " + us1.getAccount(), "\n" + us1.getNickname()};
-                        ArrayAdapter<String> adapter1 = new ArrayAdapter<String>
-                                (this, android.R.layout.simple_expandable_list_item_1, strs1);
-                        listView.setAdapter(adapter1);
-                    } else {
-                        Toast.makeText(this, "账号不存在", Toast.LENGTH_LONG).show();
-                    }
-                    break;
->>>>>>> div
-                case 1:
-                    String[] strs2 = new String[p2.size()];
-                    for (int j = 0; j < p2.size(); j++) {
-                        strs2[j] = "No" + j + "    " + p2.get(j).getName() + "  " + p2.get(j).getParkId()
-                                + "号停车场\n" + p2.get(j).getDate().toString();
-                    }
-                    ArrayAdapter<String> adapter2 = new ArrayAdapter<String>
-                            (this, android.R.layout.simple_expandable_list_item_1, strs2);
-                    listView.setAdapter(adapter2);
-                    break;
-                case 2:
-                    break;
-                case 3:
-                    Intent sendIntent = new Intent(Intent.ACTION_SENDTO, Uri.parse("smsto:" + "10086"));
-                    //sendIntent.setPackage("com.android.mms");
-                    sendIntent.putExtra("sms_body", "康程华要在马路上停车");
-                    startActivity(sendIntent);
-                    break;
-                case 4:
 
-                    break;
-
-                default:
             }
-
         }
     }
-}
+
